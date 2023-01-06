c5d87f478ddece4eb25d4a249ce5e34a82b20997
shuimuliang <shuimuliang@gmail.com>
2023-01-06 17:14:49 +0800

    fix: test_merkle_proof
    ref
    https://github.com/nervosnetwork/sparse-merkle-tree/blob/master/src/tests/tree.rs
    ln 281

--- smt-proof/src/lib.rs
+++ smt-proof/src/lib.rs
@@ -11,6 +11,7 @@
 #[cfg(test)]
 mod tests {
     use {
+        blake3::{self, Hash},
         hex::decode,
         solana_sdk::pubkey::Pubkey,
         sparse_merkle_tree::{
@@ -31,6 +32,9 @@ mod tests {
     fn test_smt_new_empty_tree() {
         let tree = SMT::default();
         assert!(tree.is_empty());
+        assert_eq!(tree.store().branches_map().len(), 0);
+        assert_eq!(tree.store().leaves_map().len(), 0);
+        assert_eq!(tree.root(), &H256::zero());
     }
 
     #[test]
@@ -44,5 +48,39 @@ mod tests {
         let res2 = tree.get(&key);
         assert!(res2.is_ok());
         assert_eq!(val, res2.unwrap());
+    }
+
+    /// convert solana Pubkey to H256
+    fn pubkey_to_h256(key: &Pubkey) -> H256 {
+        H256::from(key.to_bytes())
+    }
+
+    /// convert solana hash to H256
+    fn blake3hash_to_h256(hash: &Hash) -> H256 {
+        H256::from(*hash.as_bytes())
+    }
+
+    #[test]
+    fn test_smt_build_verify() {
+        let mut tree = SMT::default();
+
+        let pub_key1 = Pubkey::new_unique();
+        let pub_key2 = Pubkey::new_unique();
+
+        let key1 = pubkey_to_h256(&pub_key1);
+        let key2 = pubkey_to_h256(&pub_key2);
+        let b1_raw = b"binary1";
+        let b2_raw = b"binary2";
+        let bl3_hash_1 = blake3::hash(b1_raw.as_slice());
+        let bl3_hash_2 = blake3::hash(b2_raw.as_slice());
+        let val1 = blake3hash_to_h256(&bl3_hash_1);
+        let val2 = blake3hash_to_h256(&bl3_hash_2);
+        tree.update(key1, val1).unwrap();
+        tree.update(key2, val2).unwrap();
+
+        let proof = tree.merkle_proof(vec![key1, key2]).expect("proof");
+        assert!(proof
+            .verify::<Blake2bHasher>(tree.root(), vec![(key1, val1), (key2, val2)])
+            .expect("verify"));
     }
 }
