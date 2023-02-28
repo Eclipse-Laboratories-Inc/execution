6e4da8e6494bb94f9510fe92d7479e22636182bc
Spartucus <spartucus@users.noreply.github.com>
2023-02-28 14:25:12 +0800

    restart solo_rpc can get latest blockstore

--- rpc/src/bin/solo_rpc.rs
+++ rpc/src/bin/solo_rpc.rs
@@ -1,18 +1,4 @@
 use {
-    solana_rpc::{
-        rpc::{create_validator_exit, JsonRpcConfig},
-        optimistically_confirmed_bank_tracker::OptimisticallyConfirmedBank,
-        rpc_service::JsonRpcService,
-        max_slots::MaxSlots,
-    },
-    solana_ledger::{
-        blockstore::{Blockstore, BlockstoreSignals},
-        blockstore_options::BlockstoreOptions,
-        blockstore_processor::{ProcessOptions},
-        bank_forks_utils,
-        leader_schedule_cache::LeaderScheduleCache,
-        genesis_utils::{create_genesis_config, GenesisConfigInfo},
-    },
     solana_client::rpc_config::RpcContextConfig,
     solana_client::{connection_cache::ConnectionCache, rpc_cache::LargestAccountsCache},
     solana_gossip::{
@@ -21,36 +7,64 @@ use {
         crds::GossipRoute,
         crds_value::{CrdsData, CrdsValue, SnapshotHashes},
     },
-    solana_runtime::{
-        hardened_unpack::{open_genesis_config, MAX_GENESIS_ARCHIVE_UNPACKED_SIZE},
-        bank::Bank, bank_forks::BankForks, commitment::BlockCommitmentCache, snapshot_config::SnapshotConfig,
+    solana_ledger::{
+        bank_forks_utils,
+        blockstore::{Blockstore, BlockstoreSignals},
+        blockstore_options::{AccessType, BlockstoreOptions},
+        blockstore_processor::{process_blockstore_from_root, ProcessOptions},
+        genesis_utils::{create_genesis_config, GenesisConfigInfo},
+        leader_schedule_cache::LeaderScheduleCache,
     },
-    
+    solana_rpc::{
+        max_slots::MaxSlots,
+        optimistically_confirmed_bank_tracker::OptimisticallyConfirmedBank,
+        rpc::{create_validator_exit, JsonRpcConfig},
+        rpc_service::JsonRpcService,
+    },
+    solana_runtime::{
+        accounts_background_service::AbsRequestSender,
+        bank::Bank,
+        bank_forks::BankForks,
+        commitment::BlockCommitmentCache,
+        hardened_unpack::{open_genesis_config, MAX_GENESIS_ARCHIVE_UNPACKED_SIZE},
+        snapshot_config::SnapshotConfig,
+    },
     solana_sdk::{
         clock::Slot,
         genesis_config::{ClusterType, DEFAULT_GENESIS_ARCHIVE},
+        hash::Hash,
         signature::Signer,
         signer::keypair::Keypair,
-        hash::Hash,
     },
-    solana_streamer::socket::SocketAddrSpace,
     solana_send_transaction_service::send_transaction_service::{self, SendTransactionService},
+    solana_streamer::socket::SocketAddrSpace,
     std::{
         collections::HashSet,
+        net::{IpAddr, Ipv4Addr, SocketAddr},
         path::{Path, PathBuf},
         sync::{
             atomic::{AtomicBool, AtomicU64, Ordering},
             Arc, RwLock,
         },
         thread::{self, Builder, JoinHandle},
-        net::{IpAddr, Ipv4Addr, SocketAddr},
+        time::{Duration, Instant},
     },
 };
 
 fn main() {
+    let start = Instant::now();
     let ledger_path = Path::new("test-ledger");
     let genesis_config = open_genesis_config(&ledger_path, MAX_GENESIS_ARCHIVE_UNPACKED_SIZE);
-    let blockstore = Arc::new(Blockstore::open(&ledger_path).unwrap());
+    let blockstore = Arc::new(
+        Blockstore::open_with_options(
+            &ledger_path,
+            BlockstoreOptions {
+                access_type: AccessType::Secondary,
+                ..BlockstoreOptions::default()
+            },
+        )
+        .unwrap(),
+    );
     let non_primary_accounts_path = blockstore.ledger_path().join("accounts");
     let account_paths = vec![non_primary_accounts_path];
     let process_options = ProcessOptions::default();
@@ -63,17 +77,26 @@ fn main() {
         ..SnapshotConfig::default()
     };
 
-    let (bank_forks, _leader_schedule_cache, ..) =
-        bank_forks_utils::load_bank_forks(
-            &genesis_config,
-            &blockstore,
-            account_paths,
-            None,
-            Some(&snapshot_config),
-            &process_options,
-            None,
-            None,
-        );
+    let (bank_forks, leader_schedule_cache, ..) = bank_forks_utils::load_bank_forks(
+        &genesis_config,
+        &blockstore,
+        account_paths,
+        None,
+        Some(&snapshot_config),
+        &process_options,
+        None,
+        None,
+    );
+    process_blockstore_from_root(
+        &blockstore,
+        &bank_forks,
+        &leader_schedule_cache,
+        &process_options,
+        None,
+        None,
+        &AbsRequestSender::default(),
+    )
+    .unwrap();
 
     let exit = Arc::new(AtomicBool::new(false));
     let validator_exit = create_validator_exit(&exit);
@@ -83,11 +106,8 @@ fn main() {
         SocketAddrSpace::Unspecified,
     ));
     let ip_addr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
-    let rpc_addr = SocketAddr::new(
-        ip_addr,
-        8899,
-    );
-    
+    let rpc_addr = SocketAddr::new(ip_addr, 9988);
+
     let mut block_commitment_cache = BlockCommitmentCache::default();
     let bank_forks_guard = bank_forks.read().unwrap();
     block_commitment_cache.set_all_slots(
@@ -101,7 +121,7 @@ fn main() {
     let connection_cache = Arc::new(ConnectionCache::default());
     let rpc_service = JsonRpcService::new(
         rpc_addr,
-        JsonRpcConfig{
+        JsonRpcConfig {
             enable_rpc_transaction_history: true,
             full_api: true,
             ..JsonRpcConfig::default()
@@ -130,7 +150,13 @@ fn main() {
         Arc::new(AtomicU64::default()),
     );
 
-    println!("rpc: {}", rpc_addr);
+    let duration = start.elapsed();
+    let last_slot = bank_forks_guard.working_bank().slot();
+    let last_root = bank_forks_guard.root();
+    println!(
+        "rpc: {}, slot: {}, root: {}, it costs {:?} to start.",
+        rpc_addr, last_slot, last_root, duration
+    );
 
     rpc_service.join().unwrap();
 }
